dbbc9e8b281fa1bfa6862c84412f771da3fa7ac8
RedPhortex <137916751+RedPhortex@users.noreply.github.com>
2024-10-07 13:38:29 +0200

    Improved build config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ import react from "@vitejs/plugin-react";
  */
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          mui: ["@mui/icons-material", "@mui/joy", "@mui/system", "@emotion/react", "@emotion/styled"],
+          lib: ["xlsx"],
+        },
+      },
+    },
+  },
 });
